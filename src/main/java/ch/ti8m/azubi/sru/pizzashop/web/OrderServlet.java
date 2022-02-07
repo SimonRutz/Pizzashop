952ad6e095146d695ab17799e643a7a829c7b891
sru <simon.rutz@ti8m.ch>
2022-02-07 15:57:27 +0100

    gradle.build -> Project can be run, Servlets are working

--- src/main/java/ch/ti8m/azubi/sru/pizzashop/web/OrderServlet.java
+++ src/main/java/ch/ti8m/azubi/sru/pizzashop/web/OrderServlet.java
@@ -1,4 +1,35 @@
 package ch.ti8m.azubi.sru.pizzashop.web;
 
-public class OrderServlet {
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+
+@WebServlet("/Order")
+public class OrderServlet extends HttpServlet {
+    public OrderServlet() throws SQLException {
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+
+        PrintWriter out = resp.getWriter();
+
+        out.println("<html><body>");
+
+        out.println("<h1>Order</h1>");
+
+        out.println("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        doGet(req, resp);
+    }
 }
--- src/main/java/ch/ti8m/azubi/sru/pizzashop/web/PizzaServlet.java
+++ src/main/java/ch/ti8m/azubi/sru/pizzashop/web/PizzaServlet.java
@@ -2,6 +2,7 @@ package ch.ti8m.azubi.sru.pizzashop.web;
 
 import ch.ti8m.azubi.sru.pizzashop.dto.Pizza;
 import ch.ti8m.azubi.sru.pizzashop.service.PizzaService;
+import ch.ti8m.azubi.sru.pizzashop.service.PizzaServiceImpl;
 import ch.ti8m.azubi.sru.pizzashop.service.PizzaServiceRegistry;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
@@ -12,48 +13,71 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.Writer;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 @WebServlet("/Pizza")
 public class PizzaServlet extends HttpServlet {
-
-    private Template template;
-
-    private PizzaService pizzaService() {
-        return PizzaServiceRegistry.getInstance().get(PizzaService.class);
+    public PizzaServlet() throws SQLException {
     }
 
-    @Override
-    public void init() throws ServletException {
-        template = new FreemarkerConfig().loadTemplate("PizzaSelection.ftl");
-    }
+    //private Template template;
+//
+    //public PizzaServlet() throws SQLException {
+    //}
+//
+    //@Override
+    //public void init() throws ServletException {
+    //    template = new FreemarkerConfig().loadTemplate("PizzaSelection.ftl");
+    //}
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
-        List<Pizza> pizzas = null;
-        try {
-            pizzas = pizzaService().list();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        //List<Pizza> pizzaList = new LinkedList<>();
+        //try {
+        //    pizzaList = pizzaService.list();
+        //} catch (SQLException e) {
+        //    e.printStackTrace();
+        //}
+//
+        //Writer writer = resp.getWriter();
+        //Map<String, Object> model = new HashMap<>();
+        //model.put("pizzaList", pizzaList);
+//
+        //try {
+        //    template.process(model, writer);
+        //} catch (TemplateException e) {
+        //    e.printStackTrace();
+        //}
 
-        Writer writer = resp.getWriter();
-        Map<String, Object> model = new HashMap<>();
-        model.put("pizzas", pizzas);
+        resp.setContentType("text/html");
 
-        try {
-            template.process(model, writer);
-        } catch (TemplateException e) {
-            e.printStackTrace();
-        }
+        PrintWriter out = resp.getWriter();
 
+        out.println("<html><body>");
+
+        out.println("<h1>Pizza</h1>");
+
+        out.println("</body></html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        //resp.sendRedirect(req.getRequestURI());
+        doGet(req, resp);
     }
 
 
+   //DataBaseConnection dataBaseConnection = new DataBaseConnection();
 
+   //PizzaServiceImpl pizzaService = new PizzaServiceImpl(dataBaseConnection.connection);
 }
